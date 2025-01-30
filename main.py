8ff7eb6575340196fc5b7002578572670a20e8f0
Cova97 <acovamartinez@gmail.com>
2025-01-30 12:53:23 -0600

    Add CORS middleware configuration to FastAPI application

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configuracion de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Permitir todas las peticiones
+    allow_credentials=True,
+    allow_methods=["*"], # Permitir todos los metodos
+    allow_headers=["*"], # Permitir todas las cabeceras
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
